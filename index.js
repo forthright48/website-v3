666a46a967cfbe7d172fc194623be572b08847c0
Mohammad Samiul Islam <forthright48@gmail.com>
2016-07-12 23:11:05 +0600

    Change index.js to export server and app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+if (require.main === module) {
+  server.listen(app.get('port'), function() {
+    console.log(`Server running at port ${app.get('port')}`);
+  });
+} else {
+  module.exports = {
+    server,
+    app
+  };
+}
