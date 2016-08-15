0e2c96e81c8c0e821def10243c1e80b0b005009f
Mohammad Samiul Islam <forthright48@gmail.com>
2016-08-15 21:51:24 +0600

    Add jquery and moment to show days since last update

--- gulp/copy.js
+++ gulp/copy.js
@@ -3,11 +3,12 @@ const rootPath = require('forthright48/world').rootPath;
 const config = require('./config.js');
 
 const filesToCopy = [
-  path.join(rootPath, 'node_modules/jquery/dist/jquery.js')
+  path.join(rootPath, 'node_modules/jquery/dist/jquery.js'),
+  path.join(rootPath, 'node_modules/moment/src/moment.js')
 ];
 
 module.exports = function(gulp) {
-  gulp.task('copy', function(done) {
+  gulp.task('copy', function() {
     return gulp.src(filesToCopy)
       .pipe(gulp.dest(config.path.vendor.js));
   });
--- src/js/intro/intro.js
+++ src/js/intro/intro.js
@@ -1,3 +1,9 @@
 /**
  * @requires ../vendors/jquery.js
+ * @requires ../vendors/moment.js
  */
+
+var update = $('.update');
+var date = update.text();
+var replace = moment(date).fromNow();
+$('.days').text(replace);
