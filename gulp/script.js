7b3a86f74d28aff9f08566c6e81ee6ccdf5941b4
Mohammad Samiul Islam <forthright48@gmail.com>
2016-08-16 14:01:00 +0600

    Add browserify

--- gulp/script.js
+++ gulp/script.js
@@ -1,48 +1,55 @@
 const path = require('path');
-const pump = require('pump');
-const resolveDependencies = require('gulp-resolve-dependencies');
 const concat = require('gulp-concat');
-const globToVinyl = require('glob-to-vinyl');
-const recursive = require('gulp-recursive-concat');
-const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const del = require('del');
 const config = require('./config.js');
 const rootPath = require('forthright48/world').rootPath;
 
-module.exports = function(gulp) {
-  let dependencyCounter;
+const browserify = require('browserify');
+const glob = require('glob');
+const source = require('vinyl-source-stream');
 
-  function solveDependency(file, len, done) {
-    const relative = path.relative(path.join(rootPath, 'src'), file);
-    pump([
-      gulp.src(file),
-      resolveDependencies({
-        pattern: /\* @requires [\s-]*(.*\.js)/g,
-        log: true
-      }),
-      concat(relative),
-      babel({
-        presets: ['es2015'],
-        plugins: ['transform-runtime']
-      }),
-      uglify(),
-      gulp.dest(config.path.dirs.public)
-    ], function() {
-      dependencyCounter++;
-      if (dependencyCounter === len) done();
+const vendors = ['jquery', 'moment'];
+
+module.exports = function(gulp) {
+
+  gulp.task('build:vendor', function() {
+    const b = browserify({
+      debug: true
     });
+
+    // require all libs specified in vendors array
+    vendors.forEach(function(lib) {
+      b.require(lib, {
+        expose: lib
+      });
+    });
+
+    return b.bundle()
+      .pipe(source('vendor.js'))
+      .pipe(gulp.dest('./public/js/vendor/'));
+  });
+
+  function browserified(filePath) {
+    console.log(filePath);
+    const fileName = path.basename(filePath);
+    return browserify(filePath)
+      .external(vendors)
+      .transform('babelify', {
+        presets: ['es2015']
+      })
+      .bundle()
+      .pipe(source(fileName));
   }
 
   gulp.task('script', function(done) {
-    dependencyCounter = 0;
-    globToVinyl(config.path.js, function(err, files) {
-      if (files.length === 0) {
-        return done();
-      }
-      for (const file in files) {
-        solveDependency(files[file].path, files.length, done);
-      }
+    const filePathArray = glob.sync('./src/js/**/*.js');
+    filePathArray.forEach(function(filePath) {
+      let destPath = path.relative(path.join(rootPath, 'src'), filePath);
+      destPath = path.join('./public', destPath);
+      destPath = path.dirname(destPath);
+      browserified(filePath)
+        .pipe(gulp.dest(destPath));
     });
+    done();
   });
 };
